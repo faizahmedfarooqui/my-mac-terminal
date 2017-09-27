307274e49b9cb7513ec9ad8b8238fa0af432def9
Faiz A Farooqui <faiz@geekyants.com>
2017-09-27 13:40:43 +0530

    Added Git Completion / Prompt, Basic Aliases with PS1 🏌

---
+++ bash-aliases.sh
@@ -0,0 +1,17 @@
+# Important: DONOT edit ls
+alias ls='ls -GFh'
+
+# PHP artisan
+alias art='php artisan'
+
+# projects
+alias projs='cd ~/Sites/projects/'
+
+alias co="git commit -am"
+alias gcam="git commit -am"
+
+alias st="git status"
+alias gst="git status"
+
+alias gaa="git add --all"
+alias gfa="git fetch --all"
---
+++ index.sh
@@ -0,0 +1,11 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source $DIR/git-completion.bash
+source $DIR/git-prompt.sh
+source $DIR/bash-aliases.sh
+
+export PS1="\[\033[0;31m\]>_\[\033[m\] \[\033[36m\]\u\[\033[m\]\[\033[0;31m\]@\[\033[32m\]\h:\[\033[33m\]\W\[\033[0;31m\]\$(__git_ps1)\[\033[m\] "
+
+export CLICOLOR=1
+
+export LSCOLORS=ExFxBxDxCxegedabagacad
